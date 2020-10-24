fc35fa68293210490c2230a2debe2a7745396997
pallaviiprabhu <69977387+pallaviiprabhu@users.noreply.github.com>
2020-10-24 16:09:40 +0530

    UI Changed, Header file completed

--- air_sim.h
+++ air_sim.h
@@ -35,3 +35,25 @@ struct airport
     int nland, nplanes, nrefuse, ntakeoff;
     struct plane pln;
 };
+
+void initqueue(struct queue*);
+void addqueue(struct queue*, struct plane);
+struct plane delqueue(struct queue*);
+int size(struct queue q);
+int empty(struct queue q);
+int full(struct queue* q);
+void initairport(struct airport* ap);
+void start(int* endtime, double* expectarrive, double* expectdepart);
+void newplane(struct airport* ap, int curtime, int action);
+void refuse(struct airport* ap, int action);
+void land(struct airport* ap, struct plane* pl, int curtime);
+void fly(struct airport* ap, struct plane* pl, int curtime);
+void idle(struct airport* ap, int curtime);
+void conclude(struct airport* ap, int endtime);
+int randomnumber(double expectedvalue);
+void apaddqueue(struct airport* ap, char type);
+struct plane apdelqueue(struct airport* ap, char type);
+int apsize(struct airport* ap, char type);
+int apfull(struct airport* ap, char type);
+int apempty(struct airport* ap, char type);
+
